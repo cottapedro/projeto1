d22e72a63786c37c7ff6b9cfd66f1cd0298c89bf
Pedro Cotta <cottamendes@gmail.com>
2018-09-19 09:54:47 -0300

    alteração requisicao front

--- projeto/src/app/auth/services/usuario.service.ts
+++ projeto/src/app/auth/services/usuario.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, RequestOptions } from '@angular/http';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import { Usuario } from 'src/app/auth/services/usuario';
 //import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch'
 import { Observable } from "rxjs/Rx";
 
-const url = 'https://5b8ebe04eb676700148a4c82.mockapi.io';
+
+const url = 'http://localhost:8080';
 //const url = 'api/usuario';
 //http(s)://5b8ebe04eb676700148a4c82.mockapi.io/:endpoint
 @Injectable()
@@ -18,16 +20,29 @@ export class UsuarioService {
   }
 
   findAllUsuarios():Observable<Usuario[]>{
-    return this.http.get(`${url}/usuarios`).map(res => res.json());
+    return this.http.get(`${url}/usuario`).map(res => res.json());
   }
 
   removerUsuario(id: number): Observable<any> {
     return this.http.delete(`${url}/usuario/${id}`);
   }
 
-//   salvarItens(anyItems: any[]): Observable<any[]>{
-//     return  this.http.post(`${url}`,JSON.stringify(anyItems)).map(res=>res.json());
-//   }
+  salvarItens(){
+    let anyItems = {'nome': 'joao', 'email': 'teste'};
+    console.log('dado que quero postar', JSON.stringify(anyItems));
+
+    let bodyString = JSON.stringify(anyItems); // Stringify payload
+        let headers      = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+        let options       = new RequestOptions({ headers: headers }); // Create a request option
+
+        return this.http.post(`${url}/usuario`, bodyString, options) // ...using post request
+                         .map(res => res.json()) // ...and calling .json() on the response to return data
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error')) //...errors if
+                         .subscribe();
+
+    //return this.http.post(`${url}/usuario`,anyItems).map(res => res.json()).subscribe();
+    //return this.http.post(`${url}/usuario`, null);
+  }
 
 //   removerItens(anyItems: any[]): Observable<any> {
 //     return this.http.delete(`${url}`, new RequestOptions({ body: JSON.stringify(anyItems) }));
